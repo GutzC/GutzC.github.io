1878e8cf38b586b6863bd84dc9a46c3d09b56d89
GutzC <camdengutzman@gmail.com>
2015-09-14 19:00:16 -0600

    Added angular-fullstack boilerplate

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gutzCGithubIoApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
